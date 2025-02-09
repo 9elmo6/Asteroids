f08cd00091817a0c80804d2e757f434c07f7a0fc
9elmo6 <am.elmoghazy@gmail.com>
2025-02-09 14:12:28 -0500

    added clock

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} ")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +14,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 if __name__ == "__main__":
     main()
